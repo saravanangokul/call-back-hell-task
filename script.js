89f962f889f32c46d043bdba953ca7876c72ad5c
saravanangokul <kksgokul43@gmail.com>
2024-04-25 06:49:21 +0530

    task complet

---
+++ script.js
@@ -0,0 +1,45 @@
+setTimeout(function name(params) {
+    const first = document.querySelector("#spn1");
+    first.textContent = "10";
+    setTimeout(function name(params) {
+      const second = document.querySelector("#spn1");
+      second.textContent = "9";
+      setTimeout(function name(params) {
+        const third = document.querySelector("#spn1");
+        third.textContent = "8";
+        setTimeout(function name(params) {
+          const fourth = document.querySelector("#spn1");
+          fourth.textContent = "7";
+          setTimeout(function name(params) {
+            const fifth = document.querySelector("#spn1");
+            fifth.textContent = "6";
+            setTimeout(function name(params) {
+              const sixth = document.querySelector("#spn1");
+              sixth.textContent = "5";
+              setTimeout(function name(params) {
+                const seven = document.querySelector("#spn1");
+                seven.textContent = "4";
+                setTimeout(function name(params) {
+                  const eight = document.querySelector("#spn1");
+                  eight.textContent = "3";
+                  setTimeout(function name(params) {
+                    const nine = document.querySelector("#spn1");
+                    nine.textContent = "2";
+                    setTimeout(function name(params) {
+                      const ten = document.querySelector("#spn1");
+                      ten.textContent = "1";
+                      setTimeout(function name(params) {
+                        const ans = (document.querySelector("#spn1").innerText =
+                          "Happy Independance Day");
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+  
